e139ab4b233a30d0e5f2b6f84fd88829b4bdef1d
Jonathan Karras <karrots@users.noreply.github.com>
2015-09-14 23:01:28 -0600

    Fixed plotting of existing data.
    Changed graphData key values from _id,v to t,v.

--- _ClientSideUI/index.php
+++ _ClientSideUI/index.php
@@ -495,7 +495,7 @@
       graphData = [];
       LOG('Got ' + rawData.data.length + ' graph data points...');
       for(var key in rawData.data)
-        graphData.push([rawData.data[key]._id, rawData.data[key].v]);
+        graphData.push([rawData.data[key].t, rawData.data[key].v]);
       graphOptions.xaxis.min = graphView.start;
       graphOptions.xaxis.max = graphView.end;
       graphOptions = $.extend(true, graphOptions, rawData.options); //http://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
